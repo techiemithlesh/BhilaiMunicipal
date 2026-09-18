c9a29f223f4eae703aebeb438648c297431334ca
techiemithlesh <techie.mithlesh@gmail.com>
2026-09-18 14:54:49 +0530

    Feat(SAF): SAF Inbox Form Update

--- Frontend/src/modules/saf/pages/Wf.jsx
+++ Frontend/src/modules/saf/pages/Wf.jsx
@@ -355,7 +355,7 @@ function Wf() {
             {/* FLOOR DETAILS */}
             <SectionCard
               title="Floor Details"
-              note="Built Up :<span> It refers to the entire carpet area along with the thickness of the external walls of the apartment. It includes the thickness of the internal walls and the columns."
+              // note="Built Up :<span> It refers to the entire carpet area along with the thickness of the external walls of the apartment. It includes the thickness of the internal walls and the columns."
               headers={[
                 "SL",
                 "Zone",
